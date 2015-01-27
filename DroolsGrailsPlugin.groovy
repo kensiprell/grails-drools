3c0abd07a7ae3597ed31ad2cff05458d86e231f5
Ken Siprell <ken.siprell@gmail.com>
2015-01-27 05:32:01 +0100

    update issues link

--- DroolsGrailsPlugin.groovy
+++ DroolsGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.drools.DroolsDomainClassArtefactHandler
 
 class DroolsGrailsPlugin {
 
-	def version = "0.9.0"
+	def version = "0.9.1"
 	def grailsVersion = "2.2 > *"
 	def pluginExcludes = [
 		"grails-app/conf/drools-context.xml",
